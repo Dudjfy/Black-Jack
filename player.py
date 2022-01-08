0d8a7f3f28314fd5520dfd74b77b94b70a5b727e
Dmitry Chirin <chirin.dima@gmail.com>
2022-01-08 15:05:00 +0100

    added docstrings to the Player class

--- player.py
+++ player.py
@@ -1,20 +1,26 @@
 from cards import ClassicDeck, EmptyDeck
 
 
-# Player class
 class Player:
+    """
+    Player class
+    """
     def __init__(self, balance: int):
         self.hands = [EmptyDeck()]
         self.balance = balance
         self.bet_size = 0
 
-    # Fills balance
     def fill_balance(self, amount: int):
+        """
+        Fills balance
+        """
         if amount > 0:
             self.balance += amount
 
-    # Gets bet if there is enough funds
     def get_bet(self, bet: int):
+        """
+        Gets bet if there is enough funds
+        """
         if bet > 0 and self.balance - bet >= 0:
             self.balance -= bet
             return bet
